9c2959c8dc1d3a05d5ab269547f86380afb1b7db
KAZUKI OKAYAMA <okymkzk0816@gmail.com>
2022-05-06 00:25:50 +0900

    URLによるコンポーネントの振り分け実装

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import { BrowserRouter } from 'react-router-dom'
 
 import './styles.css';
 import theme from './theme/theme';
-import { Router } from "./router/Router";
+import { Router } from "./route/Router";
 
 export const App = () => {
     return (
         <ChakraProvider theme={theme}>
             <BrowserRouter>
             <Router />
-            {/* <Button colorScheme="teal">ボタン</Button>
-            <p>aaaaaaaaaaaaa</p> */}
             </BrowserRouter>
         </ChakraProvider>
       );
